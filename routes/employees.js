d05c949f6da213c4a553f2ffede347c8fc509444
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-12 21:41:57 +0600

    setting routes and testing functions for deleting, receiving and editing

--- routes/employees.js
+++ routes/employees.js
@@ -6,9 +6,13 @@ const { auth} = require('../middleware/auth');
 const {all, add,remove,employee,edit} = require("../controllers/employees");
 
 router.get('/', auth, all)
-router.get('/:id', auth, () => console.log('get single employees'))
-router.post('/add', auth, add)
-router.post('/remove/:id', auth, () => console.log('remove employees'))
-router.put('/edit/:id', auth, () => console.log('edit employees'))
 
-module.exports = router
+router.get('/:id', auth, employee)
+
+router.post('/add', auth, add)
+
+router.post('/remove/:id', auth, remove)
+
+router.put('/edit/:id', auth, edit)
+
+module.exports = router;
