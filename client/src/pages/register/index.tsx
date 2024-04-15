86e0c1d2c46f55a54712e114ec779dc707ffb929
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-15 20:09:08 +0600

    create new page , registrations

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {Layout} from "../../components/layout";
+import {Card, Form, Row, Space, Typography} from "antd";
+import {CustomInput} from "../../components/customInput";
+import {PasswordInput} from "../../components/passwordInput/input";
+import {CustomButton} from "../../components/customButton";
+import {Link} from "react-router-dom";
+import {Paths} from "../../paths";
 
 export const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <Layout>
+            <Row align="middle" justify="center">
+                <Card title="Зарегистрируйтесь" style={{width: '30rem'}}>
+                    <Form onFinish={() => null}>
+                        <CustomInput  name='name' placeholder="Имя"/>
+                        <CustomInput type="email" name='email' placeholder="Email"/>
+                        <PasswordInput name="password" placeholder="Пароль"/>
+                        <PasswordInput name="confirmPassword" placeholder="Повторите пароль"/>
+                        <CustomButton type="primary" htmlType="submit">
+                            Зарегистрироваться
+                        </CustomButton>
+                    </Form>
+                    <Space direction='vertical' size='large'>
+                        <Typography.Text>
+                            Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
+        </Layout>
     );
 };
 
