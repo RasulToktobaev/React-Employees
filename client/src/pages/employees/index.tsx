4ab76b7e6fd1b60acb0481caa234da3c317bcbaf
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-17 20:36:57 +0600

    create table from antd

--- client/src/pages/employees/index.tsx
+++ client/src/pages/employees/index.tsx
@@ -2,14 +2,55 @@ import React from 'react';
 import {Layout} from "../../components/layout";
 import {CustomButton} from "../../components/customButton";
 import {PlayCircleOutlined} from "@ant-design/icons";
+import {Table} from "antd";
+import {useGetAllEmployeesQuery} from "../../app/services/employees";
+import type {ColumnsType} from "antd/es/table";
+import {Employee} from "@prisma/client";
+import {useNavigate} from "react-router-dom";
+import {Paths} from "../../paths";
 
-export  const Employees = () => {
+
+const columns: ColumnsType<Employee> = [
+    {
+        title: 'Имя',
+        dataIndex: 'firstName',
+        key: 'firstName'
+    },
+    {
+        title: 'Возраст',
+        dataIndex: 'age',
+        key: 'age'
+    },
+    {
+        title: 'Адрес',
+        dataIndex: 'address',
+        key: 'address'
+    },
+
+]
+
+export const Employees = () => {
+    const navigate = useNavigate();
+    const {data, isLoading} = useGetAllEmployeesQuery();
+
     return (
-       <Layout>
-           <CustomButton type="primary" onClick={() => null} icon={<PlayCircleOutlined/>}>
-               Добавить
-           </CustomButton>
-       </Layout>
+        <Layout>
+            <CustomButton type="primary" onClick={() => null} icon={<PlayCircleOutlined/>}>
+                Добавить
+            </CustomButton>
+            <Table
+                loading={isLoading}
+                dataSource={data}
+                pagination={false}
+                columns={columns}
+                rowKey={(record) => record.id}
+                onRow={(record) => {
+                    return{
+                     onClick:() =>   navigate(`${Paths.employee}/${record.id}`)
+                    }
+                }}
+            />
+        </Layout>
     );
 };
 
